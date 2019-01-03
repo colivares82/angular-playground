bf3e759770b2f5523fff578e2e77e2bb4c115714
Carlos Olivares <carlos.olivares.ve@gmail.com>
2019-01-03 19:51:12 +0100

    test

--- src/app/services/mail.service.ts
+++ src/app/services/mail.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MailService {
   messages: Array<Object>;
+  private data: Observable<Array<number>>;
+  private values: Array<number> = [];
+  private anyErrors: boolean;
+  private finished: boolean;
   constructor() {
     this.messages = [
       {id: 1, body: 'You have some email'},
@@ -15,7 +20,29 @@ export class MailService {
     ];
   }
 
-  addMessage = (message) => {
+  init() {
+    this.data = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(42);
+      }, 1000);
+
+      setTimeout(() => {
+        observer.next(43);
+      }, 2000);
+
+      setTimeout(() => {
+        observer.complete();
+      }, 3000);
+    });
+
+    const subscription = this.data.subscribe(
+      value => this.values.push(value),
+      error => this.anyErrors = true,
+      () => this.finished = true
+    );
+  }
+
+  addMessage = (message) => {shame
     const lastMessage = this.messages[this.messages.length - 1];
     message.id = lastMessage['id'] + 1;
     this.messages.push(message);
